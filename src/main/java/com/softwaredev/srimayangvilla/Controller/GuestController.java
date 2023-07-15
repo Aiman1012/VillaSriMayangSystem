2447457bb07e6d25ae38fe31dd73848ef01cc6db
Aiman Aeffil <aimanaeffil1012@gmail.com>
2023-07-15 09:37:38 +0800

    Repair Commit

--- src/main/java/com/softwaredev/srimayangvilla/Controller/GuestController.java
+++ src/main/java/com/softwaredev/srimayangvilla/Controller/GuestController.java
@@ -1,10 +1,23 @@
 package com.softwaredev.srimayangvilla.Controller;
 
+import com.softwaredev.srimayangvilla.Model.Package;
+import com.softwaredev.srimayangvilla.Model.Room;
+import com.softwaredev.srimayangvilla.Service.PackageService;
+import com.softwaredev.srimayangvilla.Service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class GuestController {
+
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private PackageService packageService;
 
     @GetMapping("/guestDashboard")
     public String showGuestDashboard(){
@@ -12,12 +25,20 @@ public class GuestController {
     }
 
     @GetMapping("/guestDashboard/guestRoom")
-    public String showGuestRoom(){
-        return "guestRoom";
+    public String showGuestRoom(Model model){
+        List<Room> listRooms = roomService.listAllRoom();
+        model.addAttribute("ListRooms", listRooms);
+
+        return "roomGuest";
     }
 
     @GetMapping("/guestDashboard/guestPackage")
-    public String showGuestPackage(){
+    public String showGuestPackage(Model model){
+        List<Package> listPackages = packageService.listAllPackage();
+        model.addAttribute("ListPackages", listPackages);
+
         return "guestPackage";
     }
 }
+
+
